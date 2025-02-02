6aa23ba7e66b301cc2bb27e468f247354ffc832f
shaza <shazalugman@gmail.com>
2025-02-02 12:04:01 +0300

    start lab2

--- tests/.ipynb_checkpoints/test_data_loader-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from src.data_loader import load_data
-def test_load_data():
-    """
-    Tests whether the dataset is loaded correctly and is not
-    empty.
-    """
-    data = load_data()
-    assert not data.empty, "Data should not be empty"
